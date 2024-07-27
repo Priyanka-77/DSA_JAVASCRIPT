dabaeb211681aff36afe99c19cd34bb25c975952
Priyanka <priyankabangarwa0@gmail.com>
2024-07-27 20:08:58 +0530

    completed separarte chaining in hash map

--- hashMap.js
+++ hashMap.js
@@ -50,7 +50,7 @@ class myHash {
     this.bucket = b;
     this.table = [];
     for (let i = 0; i < this.bucket; i++) {
-      this.table[i] = [];                                         //[[], [], [], [], [], [], []]
+      this.table[i] = []; //[[], [], [], [], [], [], []]
     }
   }
 
@@ -90,8 +90,38 @@ class myHash {
 let h = new myHash(7);
 h.insertItem(70);
 h.insertItem(71);
-h.insertItem(56);                                                         //[[70, 56], [71], [], [], [], [], []]
+h.insertItem(56); //[[70, 56], [71], [], [], [], [], []]
 h.displayHash();
 console.log(h.searchItem(71));
 h.deleteItem(56);
 h.displayHash();
+
+/*
+Separate chaining technique in hashing allows to us to use a linked list at each hash slot to handle the problem of collisions. That is, every slot of the hash table is a linked list, so whenever a collision occurs, the element can be appened as a node to the linked list at the slot.
+In this question, we'll learn how to fill up the hash table using Separate chaining technique. Given an array (consisting of distinct integers)  and a hashtable size, you have to fill the elements of the array into a hash table of given size. 
+Example:
+Input: hashSize = 10 sizeOfArray = 6 arr[] = {92,4,14,24,44,91}
+Output:
+1->91
+2->92
+4->4->14->24->44
+Explanation: 92%10=2 so 92 goes to slot 2. 4%10=4 so 4 goes to slot 4. 14%10=4. But 4 is already occupied so we make a linked list at this position and add 14 after 4 in slot 4 and so on.
+*/
+function separateChaining(hashSize, arr, sizeOfArray) {
+  let table = [];
+  //your code here
+  for (let i = 0; i < hashSize; i++) {
+    table[i] = []; //[[], [], [], [], [], [], []]
+  }
+
+  for (let i = 0; i < sizeOfArray; i++) {
+    let index = Math.floor(arr[i] % hashSize); //hash function
+    table[index].push(arr[i]);
+  }
+  // return table;
+  for (let i = 0; i < table?.length; i++) {
+    console.log(i + " --> " + table[i].join(" --> "));
+  }
+}
+console.log("separate chaining");
+separateChaining(10, [92, 4, 14, 24, 44, 91], 6);
