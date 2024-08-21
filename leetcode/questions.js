9c3b158e3339d6b4af1987109fec35a19dbe2b25
Priyanka <priyankabangarwa0@gmail.com>
2024-08-21 22:30:46 +0530

    concat of the array leetoce question

--- leetcode/questions.js
+++ leetcode/questions.js
@@ -40,4 +40,28 @@ function containsDuplicate(nums) {
     }
   }
   return false;
-}
+};
+console.log(containsDuplicate([1, 2, 3, 4]));
+
+
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+Specifically, ans is the concatenation of two nums arrays.
+Return the array ans.
+Example 1:
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+*/
+function getConcatenation(nums) {
+    let ans = nums;
+    const len = nums?.length;
+
+    for(let i=0; i<len; i++){
+        ans[i + len] = nums[i];
+    }
+    return ans;
+};
+console.log(getConcatenation([1, 2, 1]));
