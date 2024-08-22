46ef73cfbc3648218638c01db3c38c8a75361969
Priyanka <priyankabangarwa0@gmail.com>
2024-08-22 09:16:21 +0530

    completed minimum operations questions

--- leetcode/questions.js
+++ leetcode/questions.js
@@ -40,9 +40,8 @@ function containsDuplicate(nums) {
     }
   }
   return false;
-};
+}
 console.log(containsDuplicate([1, 2, 3, 4]));
-
 
 /*
 Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
@@ -56,12 +55,34 @@ Explanation: The array ans is formed as follows:
 - ans = [1,2,1,1,2,1]
 */
 function getConcatenation(nums) {
-    let ans = nums;
-    const len = nums?.length;
+  let ans = nums;
+  const len = nums?.length;
 
-    for(let i=0; i<len; i++){
-        ans[i + len] = nums[i];
-    }
-    return ans;
-};
+  for (let i = 0; i < len; i++) {
+    ans[i + len] = nums[i];
+  }
+  return ans;
+}
 console.log(getConcatenation([1, 2, 1]));
+
+/*
+You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+Return the minimum number of operations to make all elements of nums divisible by 3.
+Example 1:
+Input: nums = [1,2,3,4]
+Output: 3
+Explanation: All array elements can be made divisible by 3 using 3 operations:
+Subtract 1 from 1.
+Add 1 to 2.
+Subtract 1 from 4.
+*/
+function minimumOperations(nums) {
+  let operationCount = 0;
+  for (let i = 0; i < nums?.length; i++) {
+    if (nums[i] % 3 !== 0) {
+      operationCount++;
+    }
+  }
+  return operationCount;
+};
+console.log(minimumOperations([1, 2, 1]));
