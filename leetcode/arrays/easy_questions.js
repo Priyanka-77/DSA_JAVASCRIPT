d3d63bb605ea1a815f169c6e6707a62eae077f3b
Priyanka <priyankabangarwa0@gmail.com>
2024-08-24 00:10:24 +0530

    updated the easy questions leetcode

--- leetcode/arrays/easy_questions.js
+++ leetcode/arrays/easy_questions.js
@@ -137,3 +137,25 @@ function findWordsContaining(words, x) {
   return characters;
 }
 console.log(findWordsContaining(["leet", "code"], "e"));
+
+/*
+7. Shuffle the Array
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn]. Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+Example 1:
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+*/
+function shuffle(nums, n) {
+  let newArr = [];
+  let i = 0;
+  let j = n;
+
+  while (i < n && j < nums?.length) {
+    newArr.push(nums[i], nums[j]);
+    i++;
+    j++;
+  }
+  return newArr;
+}
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
