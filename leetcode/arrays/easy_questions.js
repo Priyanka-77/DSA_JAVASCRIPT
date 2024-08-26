f94122d2071d25a04a88272ed401f32790536c47
Priyanka <priyankabangarwa0@gmail.com>
2024-08-26 21:49:16 +0530

    solved one easy array question

--- leetcode/arrays/easy_questions.js
+++ leetcode/arrays/easy_questions.js
@@ -263,3 +263,29 @@ function kidsWithCandies(candies, extraCandies) {
   return greatestCandies;
 }
 console.log(kidsWithCandies([2, 8, 7], 1));
+
+/*
+11. Count Pairs Whose Sum is Less than Target.
+Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+Example 1:
+Input: nums = [-1,1,2,3,1], target = 2
+Output: 3
+Explanation: There are 3 pairs of indices that satisfy the conditions in the statement:
+- (0, 1) since 0 < 1 and nums[0] + nums[1] = 0 < target
+- (0, 2) since 0 < 2 and nums[0] + nums[2] = 1 < target 
+- (0, 4) since 0 < 4 and nums[0] + nums[4] = 0 < target
+Note that (0, 3) is not counted since nums[0] + nums[3] is not strictly less than the target.
+*/
+function countPairs(nums, target) {
+  let count = 0;
+
+  for (let i = 0; i < nums?.length; i++) {
+    for (let j = i + 1; j < nums?.length; j++) {
+      if (nums[i] + nums[j] < target) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+console.log(countPairs([-6, 2, 5, -2, -7, -1, 3], -2));
