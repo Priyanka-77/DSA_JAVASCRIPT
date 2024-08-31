db7c97921975804c162683562177f5cad6e1ce19
Priyanka <priyankabangarwa0@gmail.com>
2024-08-31 22:05:17 +0530

    completed array leetcode question

--- leetcode/arrays/easy_questions.js
+++ leetcode/arrays/easy_questions.js
@@ -318,3 +318,26 @@ function smallerNumbersThanCurrent(nums) {
   return outputRes;
 }
 console.log(smallerNumbersThanCurrent([6, 5, 4, 8]));
+
+/*
+13. Find the Number of Good Pairs I
+You are given 2 integer arrays nums1 and nums2 of lengths n and m respectively. You are also given a positive integer k.A pair (i, j) is called good if nums1[i] is divisible by nums2[j] * k (0 <= i <= n - 1, 0 <= j <= m - 1). Return the total number of good pairs.
+Example 1:
+Input: nums1 = [1,3,4], nums2 = [1,3,4], k = 1
+Output: 5
+Explanation:
+The 5 good pairs are (0, 0), (1, 0), (1, 1), (2, 0), and (2, 2).
+*/
+function numberOfPairs(nums1, nums2, k) {
+  let count = 0;
+
+  for (let i = 0; i < nums1?.length; i++) {
+    for (let j = 0; j < nums2?.length; j++) {
+      if (nums1[i] % (nums2[j] * k) === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+console.log(numberOfPairs([1, 2, 4, 12], [2, 4], 3));
