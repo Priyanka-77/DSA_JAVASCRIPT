baf6c2ec1be3bd17cd32102c0882ca4378851aa0
Priyanka <priyankabangarwa0@gmail.com>
2025-01-06 20:25:37 +0530

    completed  queue implementation using linked list

--- queues.js
+++ queues.js
@@ -6,16 +6,20 @@ class Queue {
     this.items = [];
   }
 
-  pushElement(ele) {
+  enqueue(ele) {
     this.items.push(ele);
   }
 
-  popElement() {
+  dequeue() {
     this.items.shift();
   }
 
-  peekElement() {
+  front() {
     return this.items[0];
+  }
+
+  rear() {
+    return this.items[this.items?.length - 1];
   }
 
   isEmpty() {
@@ -35,15 +39,67 @@ class Queue {
   }
 }
 const stack = new Queue();
-stack.pushElement(10);
-stack.pushElement(20);
-stack.pushElement(30);
-stack.pushElement(40);
+stack.enqueue(10);
+stack.enqueue(20);
+stack.enqueue(30);
+stack.enqueue(40);
 stack.print();
-stack.popElement();
+stack.dequeue();
 stack.print();
 console.log(stack.isEmpty());
-stack.peekElement();
+stack.front();
 console.log(stack.size());
 stack.clear();
 stack.print();
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class QueueLL {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
+  }
+
+  enqueueLL(x) {
+    let newNode = new Node(x);
+    if (this.front === null) {
+      this.front = this.rear = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
+    this.size++;
+  }
+
+  dequeueLL() {
+    if (this.front === null) return null;
+    let res = this.front.data;
+    this.front = this.front.next;
+    this.size--;
+    return res;
+  }
+
+  isEmptyLL() {
+    return this.size === 0;
+  }
+
+  sizeLL() {
+    return this.size;
+  }
+}
+console.log(`----------Linked list implemenatation of Queue---------------`);
+const stackLL = new QueueLL();
+stackLL.enqueueLL(10);
+stackLL.enqueueLL(20);
+stackLL.enqueueLL(30);
+stackLL.enqueueLL(40);
+stackLL.dequeueLL();
+console.log(stackLL.isEmptyLL());
+console.log(stackLL.sizeLL());
+stackLL.printLL();
